9cd886b3835ff0a30d78f22ebcbadbf58bf98914
marwa900 <moussamarwa519@gmail.com>
2025-08-20 19:36:16 +0300

    deleted review doctypes

--- medis/medis/doctype/review_scan_item/review_scan_item.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Marwa and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class ReviewScanItem(Document):
-	pass
