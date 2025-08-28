30b860e5b32d89c4e55b6fba4acde139d7b891a0
Hasan Haj Hasan <hasanhajhasan98@gmail.com>
2025-08-28 16:24:32 +0300

    chore: remove obsolete custom field and workflow scripts

--- medis/medis/scripts/after_install.py
+++
@@ -1,9 +0,0 @@
-def after_install():
-    from medis.medis.scripts.workflows.insert_sales_invoice_workflow import insert_workflow
-    from medis.medis.scripts.custom_field.import_custom_field import insert_custom_field_from_file
-    from medis.medis.scripts.custom_roles.add_roles import add_roles_from_json
-    
-    add_roles_from_json()
-    insert_custom_field_from_file()
-    insert_workflow()
-    
