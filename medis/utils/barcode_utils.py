938b7670291452ce15ab41216261a915a7d9f381
Hasan Haj Hasan <hasanhajhasan98@gmail.com>
2025-09-12 11:01:01 +0300

    fix: fix barcode generation

--- medis/utils/barcode_utils.py
+++ medis/utils/barcode_utils.py
@@ -4,30 +4,25 @@ from barcode.writer import ImageWriter
 from io import BytesIO
 import base64
 from PIL import Image
-
 @frappe.whitelist()
 def generate_barcode(*args, **kwargs):
     # Remove unwanted * before generating
     docname = (kwargs.get("docname") or args[0]).replace("*", "")
     CODE128 = barcode.get_barcode_class("code128")
-
     buffer = BytesIO()
     options = {
         "module_width": 0.15,   # narrower bars
         "module_height": 5,    # shorter height
         "quiet_zone": 1,        # less margin
-        "font_size": 12,         # no text
+        "font_size": 0,         # no text
         "text_distance": 0,
         "background": "white",
         "foreground": "black"
     }
-
     CODE128(docname, writer=ImageWriter()).write(buffer, options)
-
     # Convert to transparent background
     buffer.seek(0)
     img = Image.open(buffer).convert("RGBA")
-
     datas = img.getdata()
     newData = []
     for item in datas:
@@ -36,9 +31,7 @@ def generate_barcode(*args, **kwargs):
         else:
             newData.append(item)
     img.putdata(newData)
-
     buffer_out = BytesIO()
     img.save(buffer_out, format="PNG")
-
     encoded = base64.b64encode(buffer_out.getvalue()).decode()
     return f"data:image/png;base64,{encoded}"
